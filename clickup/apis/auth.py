7a2b52d9eabed57978e2b0865b8e59ada4cf6fab
builder-avinash <builderavinash@gmail.com>
2022-02-05 11:13:50 +0530

    added method to get access token from python, this should later be done in client

--- clickup/apis/auth.py
+++ clickup/apis/auth.py
@@ -1,12 +1,77 @@
 import requests
+import webbrowser
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib.parse import urlparse, parse_qs
+from threading import Thread, Lock
+from os.path import exists
 
-redirect_uri='https://app.clickup.com'
+code=''
+mutex = Lock()
 
 def getAuthToken(client_id, client_secret):
-    urlForCode = 'https://app.clickup.com/api?client_id={client_id}&redirect_uri={redirect_uri}/20529126'
-    # requests.post(urlForCode, headers={'host': redirect_uri})
+    global code
+    getCode(client_id)
 
-    print(requests.post(urlForCode, headers={'host': redirect_uri}).text)
+    if isValidAuthTokenPresent():
+        return getSavedAuthToken()
 
-    # urlForToken = 'https://api.clickup.com/api/v2/oauth/token?client_id={client_id}&client_secret={client_secret}&code='
-    # requests.post(request2)
+    url = f'https://api.clickup.com/api/v2/oauth/token?client_id={client_id}&client_secret={client_secret}&code={code}'
+    access_token=requests.post(url).json()['access_token']
+    print('access_token ', access_token)
+
+    saveAuthToken(access_token)
+
+    return access_token
+
+# this part would be done in client
+def getCode(client_id):
+    address='0.0.0.0'
+    port=4000
+    redirect_uri = f'http://{address}:{port}'
+
+    url = f'https://app.clickup.com/api?client_id={client_id}&redirect_uri={redirect_uri}'
+    webbrowser.get(using='chrome').open(url)
+
+    webServer = HTTPServer((address, port), clickupAuthServer)
+    thread=Thread(name='daemon_server', target=webServer.serve_forever, daemon = True)
+    thread.start()
+    mutex.acquire()
+    mutex.acquire()
+    webServer.server_close()
+
+    return ""
+
+class clickupAuthServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        global code
+        query=parse_qs(urlparse(self.path).query)
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("OK", "utf-8"))
+        if 'code' in query:
+            code = query['code'][0]
+            print('code is ', code)
+            mutex.release()
+
+def getAuthTokenFileName():
+    return 'token.out'
+
+def isValidAuthTokenPresent():
+    filename=getAuthTokenFileName()
+    if not exists(filename):
+        return False
+    token=getSavedAuthToken()
+    return True
+
+def getSavedAuthToken():
+    filename=getAuthTokenFileName()
+    if not exists(filename):
+        return ''
+    token=open(filename).read()
+    return token
+
+def saveAuthToken(access_token):
+    file=open(getAuthTokenFileName(), 'w')
+    file.write(access_token)
+    file.close()
